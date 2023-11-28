1c00461aa147bb28829ebeceebac90f14150f328
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2023-11-28 15:44:53 +0100

    overview working:)

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,27 +1,32 @@
 <script>
-  import { StructuredText } from '@datocms/svelte';
-  import { error } from '@sveltejs/kit';
-  export let data;
-  const { article } = data;
-  if (!article) {
-    throw error(404, { message: 'Article Not found' });
-  }
+  export let data
+
+  const articles = data?.allArticles || []
 </script>
-<div class="article">
 
-{#each article as article }
-  <h1> {article.titel} </h1>
-{/each}
+<div>
+  <div class="title">
+      <h1>
+          Blog Posts
+      </h1>
+  </div>
+  <div class="articles">
+      <div class="article-cards">
+          {#each articles as article}
+              <a href="blog/{article.slug}">
+                  <div class="article-card">
+                      <div class="article-title">
+                          {article.titel}
+                      </div>
 
-  <h1 class="article-title">
-    {article.titel}
-  </h1>
-  {#if article.publicationDate}
-    <div class="article-publication-date">
-      Article published on: {new Date(article.publicationDate).toLocaleString()}
-    </div>
-  {/if}
-  <div>
-    <StructuredText data={article.content} />
+                      {#if article.publicationDate}
+                          <div class="article-publication-date">
+                              Post released on: { new Date(article.publicationDate).toLocaleString() }
+                          </div>
+                      {/if}
+                  </div>
+              </a>
+          {/each}
+      </div>
   </div>
 </div>
