70215f162182875e154f078a2f63902f43ca43f5
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2023-12-06 15:57:19 +0100

    working on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <nav> 
+        <li>home</li>
+        <li>diensten</li>
+        <h2> Erica van Dooren</h2>
+        <li>blog</li>
+        <li>contacten</li>
+    </nav>
+    <h1>Erica van Dooren</h1>
+
+    <section class="introductie">
+
+    </section>
+    <section class="werkzaamheden"></section>
+    <section class="reviews"></section>
+</main>
+<style>
+
+  @import url('https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&family=Quicksand:wght@400;500;600&display=swap');
+
+    :global(body) { 
+        margin: 0;
+        padding: 0;
+    }
+
+    main{
+        background-color: #000;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100vh;
+        
+    }
+     h1{
+        font-family: 'Libre Caslon Text', serif;
+        color: #fff;
+        font-size: 10em;
+        margin-top: 1em;
+        display: flex;
+        justify-content: center;
+     }
+     h2{
+        font-family: 'Libre Caslon Text', serif;
+     }
+     nav{
+        width: 100%;
+        height: 5em;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        border: 1px solid #fff;
+        color: #fff;
+     }
+
+     li{
+        font-family: 'Quicksand', sans-serif;       
+        margin: 2em;
+        justify-content: space-evenly;
+        flex-direction: column;
+        list-style: none;
+     }
+</style>
+
